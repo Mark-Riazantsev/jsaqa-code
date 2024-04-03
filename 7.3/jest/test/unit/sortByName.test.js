3d81aa1a8ea7cd9d03b09504cea92f0274b625c9
Riazantsev <riazantsevmark327@gmail.com>
2024-04-03 21:17:22 +0400

    add tests

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -23,3 +23,33 @@ describe("Books names test suit", () => {
     expect(() => sorting.sortByName()).toThrow(TypeError)
   })
 });
+
+it("Books names should be sorted in descending order", () => {
+  const input = [
+    "Гарри Поттер",
+    "Властелин Колец",
+    "Волшебник изумрудного города",
+  ];
+
+  const expected = [
+    "Гарри Поттер",
+    "Волшебник изумрудного города",
+    "Властелин Колец",
+  ];
+
+  const output = sorting.sortByName(input, "desc");
+
+  expect(output).toEqual(expected);
+});
+
+it("Empty array should return empty array", () => {
+  const input = [];
+  const output = sorting.sortByName(input);
+
+  expect(output).toEqual([]);
+});
+
+it("Non-array input should throw an exception", () => {
+  expect(() => sorting.sortByName("invalid input")).toThrow(TypeError);
+});
+
